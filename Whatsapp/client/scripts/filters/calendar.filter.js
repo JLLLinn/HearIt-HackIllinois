433e42b0a1853d58de0bb3bc0222607ba5d8323f
Jiaxin Lin <craiglin1992@gmail.com>
2016-02-20 04:02:52 -0600

    added whatsapp sample code
    step 1 completed

---
+++ Whatsapp/client/scripts/filters/calendar.filter.js
@@ -0,0 +1,19 @@
+/**
+ * Created by jiaxinlin on 2/20/16.
+ */
+angular
+    .module('Whatsapp')
+    .filter('calendar', calendar);
+
+function calendar() {
+    return function (time) {
+        if (!time) return;
+
+        return moment(time).calendar(null, {
+            lastDay: '[Yesterday]',
+            sameDay: 'LT',
+            lastWeek: 'dddd',
+            sameElse: 'DD/MM/YY'
+        });
+    };
+}
---
+++ Whatsapp/client/scripts/routes.js
@@ -0,0 +1,26 @@
+/**
+ * Created by jiaxinlin on 2/20/16.
+ */
+angular
+    .module('Whatsapp')
+    .config(config);
+
+function config($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('tab', {
+            url: '/tab',
+            abstract: true,
+            templateUrl: 'client/templates/tabs.html'
+        })
+        .state('tab.chats', {
+            url: '/chats',
+            views: {
+                'tab-chats': {
+                    templateUrl: 'client/templates/chats.html',
+                    controller: 'ChatsCtrl as chats'
+                }
+            }
+        });
+
+    $urlRouterProvider.otherwise('tab/chats');
+}
