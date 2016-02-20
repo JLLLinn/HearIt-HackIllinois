b0d7237fa982a636dad39d4e6da86b20c6ffc28c
Edward Xue <edthemaster1@gmail.com>
2016-02-20 14:56:27 -0600

    More angular for sound feed card and updated card design

--- HearIt/client/scripts/soundFeedController.js
+++ HearIt/client/scripts/soundFeedController.js
@@ -3,5 +3,6 @@ angular.module('HearIt')
 
 function SoundFeedController($scope, $meteor){
     var self = this;
-    this.hello = "Hello World";
+    this.hello = "Hello World"; //in html we have to use {{instance.hello}}, where instance = name of controllerAs part
+    $scope.date = "November 05, 1955"; //in html we can use use {{date}}
 }
