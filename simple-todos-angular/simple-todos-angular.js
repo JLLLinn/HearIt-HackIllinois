82928d14a4546f6e80b91132b6b9723fa239d201
Edward Xue <edthemaster1@gmail.com>
2016-02-20 02:32:48 -0600

    Basic accounts

--- simple-todos-angular/simple-todos-angular.js
+++ simple-todos-angular/simple-todos-angular.js
@@ -2,21 +2,39 @@ Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isClient) {
 
+    Accounts.ui.config({
+       passwordSignupFields: "USERNAME_ONLY"
+     });
+
   // This code only runs on the client
-  angular.module('simple-todos',['angular-meteor']);
+  angular.module('simple-todos',['angular-meteor', 'accounts.ui']);
 
   angular.module('simple-todos').controller('TodosListCtrl', ['$scope', '$meteor',
     function ($scope, $meteor) {
 
-      $scope.tasks = $meteor.collection( function() {
-        return Tasks.find({}, { sort: { createdAt: -1 } })
-      }); //using $meteor service to bind collection into our scope
+        $scope.tasks = $meteor.collection(function() {
+            return Tasks.find($scope.getReactively('query'), {sort: {createdAt: -1}})
+         }); //using $meteor service to bind collection into our scope
 
       $scope.addTask = function (newTask) {
-        $scope.tasks.push( {
-          text: newTask,
-          createdAt: new Date() }
-        );
+          $scope.tasks.push( {
+              text: newTask,
+              createdAt: new Date(),             // current time
+              owner: Meteor.userId(),            // _id of logged in user
+              username: Meteor.user().username }  // username of logged in user
+          );
       };
+
+      $scope.$watch('hideCompleted', function() {
+        if ($scope.hideCompleted)
+          $scope.query = {checked: {$ne: true}};
+        else
+          $scope.query = {};
+      });
+
+      $scope.incompleteCount = function () {
+        return Tasks.find({ checked: {$ne: true} }).count();
+      };
+
     }]);
 }
