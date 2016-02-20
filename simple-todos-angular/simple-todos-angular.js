ebbfdbfad49d8089e8f9ecec9ea1dfaca4c95eee
Jiaxin Lin <craiglin1992@gmail.com>
2016-02-20 02:35:06 -0600

    whatever

--- simple-todos-angular/simple-todos-angular.js
+++ simple-todos-angular/simple-todos-angular.js
@@ -1,23 +1,53 @@
+Tasks = new Mongo.Collection('tasks');
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+    Accounts.ui.config({
+        passwordSignupFields: "USERNAME_ONLY"
+    });
+    // This code only runs on the client
+    angular.module('simple-todos', ['angular-meteor', 'accounts.ui']);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+    function onReady() {
+        angular.bootstrap(document, ['simple-todos']);
     }
-  });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+    if (Meteor.isCordova)
+        angular.element(document).on('deviceready', onReady);
+    else
+        angular.element(document).ready(onReady);
+
+
+    angular.module('simple-todos').controller('TodosListCtrl', ['$scope', '$meteor',
+        function ($scope, $meteor) {
+            $scope.tasks = $meteor.collection(function () {
+                return Tasks.find($scope.getReactively('query'), {sort: {createdAt: -1}});
+            });
+
+            $scope.addTask = function (newTask) {
+                $scope.tasks.push({
+                        text: newTask,
+                        createdAt: new Date(),             // current time
+                        owner: Meteor.userId(),            // _id of logged in user
+                        username: Meteor.user().username
+                    }  // username of logged in user
+                );
+            };
+            $scope.$watch('hideCompleted', function () {
+                if ($scope.hideCompleted)
+                    $scope.query = {checked: {$ne: true}};
+                else
+                    $scope.query = {};
+            });
+
+            $scope.incompleteCount = function () {
+                return Tasks.find({checked: {$ne: true}}).count();
+            };
+
+        }]);
+
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function () {
+        // code to run on server at startup
+    });
 }
