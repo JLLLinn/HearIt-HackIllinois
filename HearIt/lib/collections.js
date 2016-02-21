e08201b8ec61ebc98f847b5fd3fcecfd07eb481f
Jiaxin Lin <craiglin1992@gmail.com>
2016-02-21 07:49:17 -0600

    data schema for the soundposts

--- HearIt/lib/collections.js
+++ HearIt/lib/collections.js
@@ -1,1 +1,16 @@
 SoundPosts = new Mongo.Collection('soundPosts');
+/*
+
+  Those should be in the format of [
+  {
+    title:"",
+    path: "",
+    user_id: "",
+    created_at: "",
+    description: "",
+    
+
+  }
+
+]
+*/
